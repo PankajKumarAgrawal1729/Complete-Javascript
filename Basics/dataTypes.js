b6b36d04358e40c770bb4563d2288ee661bb42a5
Pankaj Kumar <52162601+PankajKumarAgrawal1729@users.noreply.github.com>
2023-12-21 15:24:42 +0000

    Datatypes Concept

---
+++ Basics/dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello"); it is not working when we use nodejs
+
+console.log(3 + 8);
+
+// Datatypes
+// 1. number => 2^53
+// 2. bigint
+// 3. string
+// 4. boolean => true/false
+// null => standalone value, it is a object type
+// undefined
+// symbol => for uniqueness
+// object
+
+console.log(typeof null);  // object
+console.log(typeof undefined);   //undefined
