ec278fd3c4f9956a0e1fb703d68a644f23930613
Pankaj Kumar <52162601+PankajKumarAgrawal1729@users.noreply.github.com>
2023-12-22 11:35:54 +0000

    Javascript operations concept

--- Basics/conversionOperation.js
+++ Basics/conversionOperation.js
@@ -48,3 +48,47 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(stringNumber);  // 33
 console.log(typeof stringNumber);   // string
+
+
+
+// ******************* Operations ****************************
+
+let value = 3;
+let negValue = -value;
+console.log(value, negValue);  // 3 -3
+
+
+console.log(2 + 2);   // 4
+console.log(2 - 2);   // 0
+console.log(2 * 2);   // 4
+console.log(2 / 2);   // 1
+console.log(2 ** 2);  // 4
+console.log(2 % 2);   // 0
+
+let str1 = "Pankaj";
+let str2 = " Kumar";
+
+let str3 = str1 + str2;
+console.log(str3);     //Pankaj Kumar
+
+console.log("1" + 2);    // 12
+console.log(1 + "2");    // 12
+console.log("1" + "2");   // 12
+
+
+console.log("1" + 2 + 2);   // 122
+console.log(1 + 2 + "2");   // 32
+
+
+console.log(true);   // true
+
+console.log(+true);  // 1
+console.log(+"");   // 0
+// console.log(true+);  // error
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+console.log(gameCounter++);  // 100
+console.log(++gameCounter);  // 102
