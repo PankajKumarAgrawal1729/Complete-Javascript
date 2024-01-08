168e433acdaba96d190b2e40e3032d4653992b8d
Pankaj Kumar <52162601+PankajKumarAgrawal1729@users.noreply.github.com>
2024-01-08 12:19:24 +0000

    Arrow Function  in JS

---
+++ Basics-Part3/arrow-fn.js
@@ -0,0 +1,47 @@
+const user = {
+    username: "Pankaj",
+    price: 999,
+
+    welcomeMessage: function () {
+        console.log(`${this.username}, welcome to website`);
+        console.log(this);   // it refer to the current context
+    }
+}
+
+// user.welcomeMessage();
+// user.username = "Ram";
+// user.welcomeMessage();
+ 
+console.log(this);  // {} in node it refer to empty object, and in browser it refer to window
+
+
+function fun(){
+    let name = "Pankaj";
+    console.log(this.name);  // undefined
+}
+
+fun();
+
+
+const fun1 = function(){
+    let name = "Pankaj";
+    console.log(this.name);  // undefined
+}
+
+fun1();
+
+const fun2 = () => {
+    let name = "Pankaj";
+    console.log(this.name);  // undefined
+}
+
+fun2();
+
+
+// const addTwo = (a, b) => {
+//     return a + b;
+// }
+
+const addTwo = (a, b) => (a + b);  // implicite return
+console.log(addTwo(34, 5));
+
